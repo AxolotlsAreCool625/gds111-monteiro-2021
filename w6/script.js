1cddc4b0097cc9b02e66e6607e1f3433a82839cf
Monteiro <jjmonteiro@email.neit.edu>
2021-11-09 13:44:20 -0500

    End of class, I'm not sure if i did anything right i don feel well

--- w6/script.js
+++ w6/script.js
@@ -1,47 +1,89 @@
-var c=document.querySelector('canvas');
-var ctx=c.getContext('2d');
+var c = document.querySelector(`canvas`);
+var ctx = c.getContext(`2d`);
 
 class GameObject
 {
-    constructor()
-    {
-        this.x = c.width/2;
-        this.y = c.height/2;
-        this.w = 100;
-        this.h = 100;
-        this.color = 'red';
-    }
+   constructor()
+   {
+      this.x = c.width/2;
+      this.y = c.height/2;
+      this.w = 100;
+      this.h = 100;
+      this.color = `lime`;
+      this.angle = 0;
+      this.vx=2;
+      this.vy=2;
+   }
 
+   drawRect()
+   {
+      ctx.save();
+         ctx.translate(this.x, this.y)
+         ctx.rotate(this.angle * Math.PI/180)
+         ctx.fillStyle = this.color
+         ctx.fillRect(-this.w/2, -this.h/2, this.w, this.h)
+         ctx.fillStyle=`black`;
+         ctx.fillRect(-5,-5,10,10)
+      ctx.restore();
+   }
 
-    drawRect()
-    {
-        ctx.save();
-            ctx.translate(this.x, this.y)
-            ctx.filStyle = this.color
-            ctx=fillRect(-50, -50, this.w, this.h)
-        ctx.restore();
-    }
+   drawCircle()
+   {
+      ctx.save();
+         ctx.fillStyle = this.color
+         ctx.translate(this.x, this.y)
+         ctx.rotate(this.angle * Math.PI/180)
+         ctx.beginPath()
+         ctx.arc(0, 0, this.w/2, 0, 360*Math.PI, true)
+         ctx.closePath()
+         ctx.fill()
+         ctx.fillStyle=`black`;
+         ctx.fillRect(-5,-5,10,10)
+      ctx.restore();
+   }
 
-    drawCircle()
-    {
-        ctx.save();
-            ctx.filStyle = this.color
-            ctx.translate(this.x, this.y)
-            ctx.beginPath()
-            ctx=arc(0, 0, this.w/2, 0, 360*Math.PI, true)
-            ctx.closePath()
-            ctx.fill()
-        ctx.restore();
-    }
+   drawTri()
+   {
+    ctx.save();
+      ctx.fillStyle = 'green';
+      ctx.translate(this.x, this.y)
+      ctx.rotate(this.angle * Math.PI/180)
+      ctx.beginPath()
+         ctx.moveTo(0+this.w/2, 0)
+         ctx.lineTo(0-this.w/2, 0+this.h/2)
+         ctx.lineTo(0-this.w/2, 0-this.h/2)
+      ctx.closePath()
+      ctx.fill()
+      ctx.fillStyle=`black`;
+      ctx.fillRect(-5,-5,10,10)
+    ctx.restore();
+   }
 
+   move()
+   {
+       this.x += this.vx
+       this.y += this.vy
+   }
 
 }
 
 var jay = new GameObject();
-jay.color = 'blue'
-jay.drawRect();
+jay.vx==0
+jay.vy=0
 
-var bill = new GameObject();
-bill.color = 'green'
-bill.x = 200
-bill.drawRect();
+var timer = window.requestAnimationFrame(main)
+
+function main()
+{
+   ctx.clearRect(0,0,c.width, c.height)
+
+   jay.move()
+
+   if(jay.x > c.width + jay.w/2)
+   {
+       jay.x = 0 - jay.w/2
+   }
+
+   jay.drawRect();
+   timer = window.requestAnimationFrame(main) 
+}
