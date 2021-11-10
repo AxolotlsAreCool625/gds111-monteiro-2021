c7cd809647788a837a0590dbbfeb5c283eb1a62a
Monteiro <jjmonteiro@email.neit.edu>
2021-11-10 13:55:00 -0500

    End of class week 6 day two

--- w6/w6.11/GameObject.js
+++ w6/w6.11/GameObject.js
@@ -1,6 +1,3 @@
-var c = document.querySelector(`canvas`);
-var ctx = c.getContext(`2d`);
-
 class GameObject
 {
    constructor()
@@ -9,10 +6,20 @@ class GameObject
       this.y = c.height/2;
       this.w = 100;
       this.h = 100;
-      this.color = `lime`;
+      this.color = `red`;
       this.angle = 0;
-      this.vx=2;
-      this.vy=2;
+      this.vx=0;
+      this.vy=0;
+      this.img;
+   }
+
+   drawImage()
+   {
+      ctx.save();
+        ctx.translate(this.x, this.y)
+        ctx.rotate(this.angle * Math.PI/180)
+        ctx.drawImage(this.img, -this.w/2, -this.h/2, this.w, this.h)
+      ctx.restore();
    }
 
    drawRect()
@@ -44,46 +51,23 @@ class GameObject
 
    drawTri()
    {
-    ctx.save();
-      ctx.fillStyle = 'green';
-      ctx.translate(this.x, this.y)
-      ctx.rotate(this.angle * Math.PI/180)
-      ctx.beginPath()
-         ctx.moveTo(0+this.w/2, 0)
-         ctx.lineTo(0-this.w/2, 0+this.h/2)
-         ctx.lineTo(0-this.w/2, 0-this.h/2)
-      ctx.closePath()
-      ctx.fill()
-      ctx.fillStyle=`black`;
-      ctx.fillRect(-5,-5,10,10)
-    ctx.restore();
+      ctx.save()
+         ctx.fillStyle = this.color
+         ctx.translate(this.x, this.y)
+         ctx.rotate(this.angle * Math.PI/180)
+         ctx.beginPath()
+            ctx.moveTo(0+this.w/2, 0)
+            ctx.lineTo(0-this.w/2, 0+this.h/2)
+            ctx.lineTo(0-this.w/2, 0-this.h/2)
+         ctx.closePath()
+         ctx.fill()
+         ctx.fillStyle=`black`;
+         ctx.fillRect(-5,-5,10,10)
+      ctx.restore()
    }
-
    move()
    {
-       this.x += this.vx
-       this.y += this.vy
+      this.x += this.vx;
+      this.y += this.vy;
    }
-
-}
-
-var jay = new GameObject();
-jay.vx==0
-jay.vy=0
-
-var timer = window.requestAnimationFrame(main)
-
-function main()
-{
-   ctx.clearRect(0,0,c.width, c.height)
-
-   jay.move()
-
-   if(jay.x > c.width + jay.w/2)
-   {
-       jay.x = 0 - jay.w/2
-   }
-
-   jay.drawRect();
-   timer = window.requestAnimationFrame(main) 
 }
