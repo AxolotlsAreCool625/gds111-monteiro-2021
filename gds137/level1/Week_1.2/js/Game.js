5da1cbbfefad6a1d78a6938f8b725478aef1c68d
AxolotlsAreCool625 <91974187+AxolotlsAreCool625@users.noreply.github.com>
2023-03-30 09:51:16 -0400

    e

--- gds137/level1/Week_1.2/js/Game.js
+++ gds137/level1/Week_1.2/js/Game.js
@@ -5,14 +5,15 @@ var context;
 var timer;
 //1000 ms or 1 second / FPS
 var interval = 1000/60;
-var player;
+var ball;
 
 	//Set Up the Canvas
 	canvas = document.getElementById("canvas");
 	context = canvas.getContext("2d");	
 	
-	//Instantiate the Player
-	player = new Player();
+	//Instantiate the Ball
+	ball = new Ball();
+	ball.vx = 2;
 	
 	//Set the Animation Timer
 	timer = setInterval(animate, interval);
@@ -22,9 +23,9 @@ function animate()
 	//Erase the Screen
 	context.clearRect(0,0,canvas.width, canvas.height);	
 	
-	//Move the Player
-	player.x += 2;
+	//Move the ball
+	ball.x += ball.vx;
 	
 	//Update the Screen
-	player.draw();
+	ball.draw();
 }
