b74e257aa85f461adfd811bc415908a09756439a
AxolotlsAreCool625 <91974187+AxolotlsAreCool625@users.noreply.github.com>
2023-04-13 08:12:59 -0400

    awesrfvdgbtrsef

---
+++ gds137/level3/week_3.1/js/Game.js
@@ -0,0 +1,180 @@
+var canvas;
+var context;
+var interval = 1000/60;
+
+canvas = document.getElementById("canvas");
+context = canvas.getContext("2d");	
+
+timer = setInterval(animate, interval);
+
+var player1 = new GameObject();
+player1.height = 220;
+player1.width = 20;
+player1.x = canvas.width/16
+player1.y = canvas.height/2;
+
+player1.drawRect();
+
+var player2 = new GameObject();
+player2.height = 220;
+player2.width = 20;
+player2.x = canvas.width* (15/16)
+player2.y = canvas.height/2;
+player2.color = '#00f';
+
+player2.drawRect();
+
+var ball = new GameObject();
+ball.width = 40;
+ball.height = 40;
+ball.vx = 6;
+
+ball.drawCircle();
+
+function animate()
+{
+	//Erase the Screen
+	context.clearRect(0,0,canvas.width, canvas.height);	
+
+	//ball movement
+
+	ball.x += ball.vx;
+	ball.y += ball.vy;
+
+	if(ball.x < 0 || ball.x > canvas.width - ball.width/2)
+	{
+		ball.x = canvas.width/2;
+		ball.color = "#ff0000";
+	}
+
+	if(ball.y < 0 + ball.height/2 || ball.y > canvas.height - ball.height/2)
+	{
+		ball.vy = -ball.vy;
+	}
+
+	ball.drawCircle();
+	context.strokeRect(ball.x- ball.width/2, ball.y - ball.height/2, ball.width, ball.height)
+
+	//player movement
+	if(w) {
+		if (player1.y > (player1.height/2)) {
+			player1.y += -4;
+		}
+		else {
+			player1.y = (player1.height/2)
+		}
+	}
+
+
+	if(s) {
+		if (player1.y < (canvas.height - player1.height/2)) {
+			player1.y += 4;
+		}
+		else {
+			player1.y = (canvas.height - player1.height/2)
+		}
+	}
+
+	//player 2 movement
+	if(up) {
+		if (player2.y > (player2.height/2)) {
+			player2.y += -4;
+		}
+		else {
+			player2.y = (player2.height/2)
+		}
+	}
+
+
+	if(down) {
+		if (player2.y < (canvas.height - player2.height/2)) {
+			player2.y += 4;
+		}
+		else {
+			player2.y = (canvas.height - player2.height/2)
+		}
+	}
+
+
+	player1.drawRect();
+	player2.drawRect();
+
+	if(ball.left() < player1.right() && ball.right() > player1.left() && ball.top() < player1.bottom() && ball.bottom() > player1.top() )
+	{
+		collision = "true";
+	}
+	else
+	{
+		collision = "false";
+	}
+
+	if(ball.left() < player2.right() && ball.right() > player2.left() && ball.top() < player2.bottom() && ball.bottom() > player2.top() )
+	{
+		collision2 = "true";
+	}
+	else
+	{
+		collision2 = "false";
+	}
+
+	//Secetion Drawing
+	context.strokeRect(player1.x- player1.width/2, player1.y - player1.height/2, player1.width, player1.height/3)
+	context.strokeRect(player1.x- player1.width/2, player1.y - player1.height/6, player1.width, player1.height/3)
+	context.strokeRect(player1.x- player1.width/2, player1.y + player1.height/6, player1.width, player1.height/3)
+
+	context.strokeRect(player2.x- player2.width/2, player2.y - player2.height/2, player2.width, player2.height/3)
+	context.strokeRect(player2.x- player2.width/2, player2.y - player2.height/6, player2.width, player2.height/3)
+	context.strokeRect(player2.x- player2.width/2, player2.y + player2.height/6, player2.width, player2.height/3)
+
+
+	if (collision == "true")
+	{
+		if (ball.y < player1.y - player1.height/6) 
+		{
+			ball.x += player1.width
+			ball.vx = -ball.vx
+			ball.vy = -6
+			console.log("Collision Top");
+		}
+		else if (ball.y > player1.y + player1.height/6)
+		{
+			ball.x += player1.width
+			ball.vx = -ball.vx
+			ball.vy = 6
+			console.log("Collision Bottom");
+		}
+		else
+		{
+			ball.x += player1.width
+			ball.vx = -ball.vx
+			console.log("Collision Middle");
+		}
+			ball.color = "#00ff55";
+	}
+
+	if (collision2 == "true")
+	{
+		if (ball.y < player2.y - player2.height/6) 
+		{
+			ball.x -= player2.width
+			ball.vx = -ball.vx
+			ball.vy = -6
+			console.log("Collision 2 Top");
+		}
+		else if (ball.y > player2.y + player2.height/6)
+		{
+			ball.x -= player2.width
+			ball.vx = -ball.vx
+			ball.vy = 6
+			console.log("Collision 2 Bottom");
+		}
+		else
+		{
+			ball.x -= player2.width
+			ball.vx = -ball.vx
+			console.log("Collision 2 Middle");
+		}
+			ball.color = "#00ff55";
+	}
+
+}
--- gds137/level3/week_3.1/js/controls.js
+++ gds137/level3/week_3.1/js/controls.js
@@ -1,6 +1,8 @@
 //Define Booleans for each key
-var a = false;
-var d = false;
+var w = false;
+var s = false;
+var up = false;
+var down = false;
 
 //Add Event Listeners
 document.addEventListener("keydown", press);
@@ -12,13 +14,21 @@ function press(e)
 	//---This logs key codes into the browser's console.
 	//console.log("Pressed" + e.keyCode);
 	
-	if(e.keyCode == 65)
+	if(e.keyCode == 87)
 	{
-		a = true;
+		w = true;
 	}
-	if(e.keyCode == 68)
+	if(e.keyCode == 83)
 	{
-		d = true;
+		s = true;
+	}
+	if(e.keyCode == 38)
+	{
+		up = true;
+	}
+	if(e.keyCode == 40)
+	{
+		down = true;
 	}
 }
 
@@ -27,12 +37,20 @@ function release(e)
 	//---This logs key codes into the browser's console.
 	//console.log("Released" + e.keyCode);
 	
-	if(e.keyCode == 65)
+	if(e.keyCode == 87)
 	{
-		a = false;
+		w = false;
 	}
-	if(e.keyCode == 68)
+	if(e.keyCode == 83)
 	{
-		d = false;
+		s = false;
+	}
+	if(e.keyCode == 38)
+	{
+		up = false;
+	}
+	if(e.keyCode == 40)
+	{
+		down = false;
 	}
 }
